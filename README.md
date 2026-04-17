73c9c714aad102550c569e5bb5fd16aaf6f86ff7
safwan <safwan@mac.communityfibre.co.uk>
2026-04-17 22:44:15 +0100

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# smart-campus-api
+# Smart Campus API
+
+## Module
+Client-Server Architectures
+
+## Overview
+This project is a REST API built using JAX-RS for a Smart Campus system.
+
+## Technologies Used
+- Java
+- JAX-RS
+- Maven
+- NetBeans 18
---
+++ src/main/java/com/mycompany/smart/campus/api/SmartCampusApi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smart.campus.api;
+
+/**
+ *
+ * @author safwan
+ */
+public class SmartCampusApi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
